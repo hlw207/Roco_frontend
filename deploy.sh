a9ca79ea3e59b17a4e30ba748f2f9d436deac886
HeLiwei <211250207@smail.nju.edu.cn>
2024-09-26 21:55:46 +0800

    shell add

---
+++ deploy.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+name="roco_frontend"
+
+docker container stop ${name}
+docker rm ${name}
+docker rmi ${name}:latest
+echo "卸载缓存成功"
+
+docker build -t ${name} .
+echo "镜像构建成功"
+
+docker run -d -p 5174:5174 --name ${name} ${name}
